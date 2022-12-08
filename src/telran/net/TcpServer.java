61c1eeeeb7b527c41395cd295125c59e0f6cd82d
Yuragranovsky <yuragranovsky@gmail.com>
2022-12-08 15:06:47 +0200

    HW #46 ThreadsPool applying for TCP server

--- src/telran/net/TcpServer.java
+++ src/telran/net/TcpServer.java
@@ -1,15 +1,22 @@
 package telran.net;
 import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.io.*;
 public class TcpServer implements Runnable {
-   private ServerSocket serverSocket;
+   private static final int DEFAULT_N_THREADS = 5;
+private ServerSocket serverSocket;
    private int port;
    private ApplProtocol protocol;
-   public TcpServer(int port, ApplProtocol protocol) throws Exception{
+   private ExecutorService executor; 
+   public TcpServer(int port, ApplProtocol protocol, int nThreads) throws Exception{
 	   this.port = port;
 	   this.protocol = protocol;
-	   
+	   executor = Executors.newFixedThreadPool(nThreads);
 	   serverSocket = new ServerSocket(port);
+   }
+   public TcpServer(int port, ApplProtocol protocol) throws Exception {
+	   this(port, protocol, DEFAULT_N_THREADS);
    }
 	@Override
 	public void run() {
@@ -18,8 +25,7 @@ public class TcpServer implements Runnable {
 			while(true) {
 				Socket socket = serverSocket.accept();
 				TcpClientServer clientServer = new TcpClientServer(socket, protocol);
-				Thread thread = new Thread(clientServer);
-				thread.start();
+				executor.execute(clientServer);
 			}
 		} catch(Exception e) {
 			System.out.println(e.getMessage());
